5f1961383aead3ac5c15a94aae89548e62c6680d
israel <israel.bejarano.a@gmail.com>
2020-04-25 11:24:54 +0200

    commit inicial

---
+++ microservicios-commons/src/main/java/com/curso/microservicios/commons/MicroserviciosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosCommonsApplication.class, args);
+	}
+
+}
---
+++ microservicios-commons/src/test/java/com/curso/microservicios/commons/MicroserviciosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
